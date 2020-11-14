3d51177db57e5d6947d50d40a22aac27ff2ed6cd
flyme2bluemoon <matthew.zh.shen@gmail.com>
2020-11-14 14:50:04 -0500

    Create get_data.sh
    Run get_data.sh to pull the latest dataset from the Government of Ontario Data Catalogue

---
+++ get_data.sh
@@ -0,0 +1,2 @@
+wget https://data.ontario.ca/dataset/b1fef838-8784-4338-8ef9-ae7cfd405b41/resource/8b6d22e2-7065-4b0f-966f-02640be366f2/download/schoolsactivecovid.csv
+mv schoolsactivecovid.csv csv/schoolsactivecovid.csv

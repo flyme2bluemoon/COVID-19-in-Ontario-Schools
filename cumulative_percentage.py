1cbeded3d872b17c680a62bba142361421df805f
flyme2bluemoon <matthew.zh.shen@gmail.com>
2020-11-23 12:37:13 -0500

    Update csv/schoolsactivecovid.csv

--- cumulative_percentage.py
+++ cumulative_percentage.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import date
 
 school_list = []
 number_of_schools = 4828
@@ -13,6 +14,8 @@ with open("csv/schoolsactivecovid.csv", "r", encoding = "ISO-8859-1") as data:
 school_list.pop(0)
 
 print(school_list)
+today = date.today()
+print("Today's date:", today)
 print(f"Cumulative Schools with COVID-19: {len(school_list)}")
 print(f"Total schools: {number_of_schools}")
 print(f"Cumulative percentage of Schools with COVID-19: {round(((len(school_list)/number_of_schools)*100), 2)}%")
